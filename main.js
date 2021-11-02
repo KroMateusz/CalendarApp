5dff6bb3a272af988d1a1b76aee0063fc75fe10a
KroMateusz <81087684+KroMateusz@users.noreply.github.com>
2021-11-02 01:03:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const monthYear = document.querySelector('.month-year');
+const menu = document.querySelector('nav');
+const days = document.querySelector('.days');
+const previousMonth = document.querySelector('.previous');
+const nextMonth = document.querySelector('.next');
